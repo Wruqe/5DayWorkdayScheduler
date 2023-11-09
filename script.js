aad884ccbe47dd45484c06948ecf815dfccf8cfe
Ernest Jones <ernest.jones30@yahoo.com>
2023-11-08 22:27:54 -0500

    added local storage!

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
-var currentDate = dayjs().format("MMMM D, YYYY");
-document.getElementById("currentDay").textContent = currentDate;
-
 // Get the current hour in 24-hour format
 var currentHour = dayjs().hour();
 
-// Iterate through each time block
-var blocks = document.querySelectorAll(".time-block");
-blocks.forEach(function (block) {
-  var blockHour = parseInt(block.id.split("-")[1]);
+// Iterate through each time timeBlock
+var timeBlocks = document.querySelectorAll(".time-block");
+timeBlocks.forEach(function (timeBlock) {
+  var timeBlockHour = parseInt(timeBlock.id.split("-")[1]);
 
-  if (blockHour < currentHour) {
-    block.classList.add("past");
-  } else if (blockHour === currentHour) {
-    block.classList.add("present");
+  if (timeBlockHour < currentHour) {
+    timeBlock.classList.add("past");
+  } else if (timeBlockHour === currentHour) {
+    timeBlock.classList.add("present");
   } else {
-    block.classList.add("future");
+    timeBlock.classList.add("future");
   }
 });
 
+// Load saved user input from localStorage
+var savedInput = JSON.parse(localStorage.getItem("calendar"));
+if (savedInput) {
+  timeBlocks.forEach(function (timeBlock) {
+    var timeBlockId = timeBlock.id;
+    var textarea = timeBlock.querySelector("textarea");
+    if (savedInput[timeBlockId]) {
+      textarea.value = savedInput[timeBlockId];
+    }
+  });
+}
 
+// Save user input to localstorage when the save button is clicked
+timeBlocks.forEach(function (timeBlock) {
+  var saveButton = timeBlock.querySelector(".saveBtn");
+  var textarea = timeBlock.querySelector("textarea");
+  saveButton.addEventListener('click', function () {
+    var timeBlockId = timeBlock.id;
+    var userInput = textarea.value.trim();
 
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
+    var savedInput = JSON.parse(localStorage.getItem("calendar")) || {};
+    savedInput[timeBlockId] = userInput;
+    localStorage.setItem("calendar", JSON.stringify(savedInput));
+  });
+});
+// Get the current date using Day.js
+var date = dayjs().format("MMMM D, YYYY");
+
+// Find the element where you want to display the date (assuming it has an id of 'current-date')
+var currentDateElement = document.getElementById("currentDay");
+
+// Set the innerHTML of the element to display the current date
+if (currentDateElement) {
+  currentDateElement.innerHTML = "Today is " + date;
+}
